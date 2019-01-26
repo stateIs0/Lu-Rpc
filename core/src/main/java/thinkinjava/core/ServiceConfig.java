b03459d778f462d59d6f780b9d376528c8f901ce
stateis0 <cxstateis0@gmail.com>
2019-01-26 11:05:15 +0800

    add annotation.

--- core/src/main/java/thinkinjava/core/ServiceConfig.java
+++ core/src/main/java/thinkinjava/core/ServiceConfig.java
@@ -4,6 +4,8 @@ import thinkinjava.rpc.remoting.Url;
 
 /**
  *
+ * 服务配置类, 无论是客户端还是服务端, 通常需要接口名称, 设置接口类型, 以及接口 URL.
+ *
  * @author 莫那·鲁道
  * @date 2018/10/15-下午4:23
  */
---
+++ core/src/main/java/thinkinjava/event/package-info.java
@@ -0,0 +1,9 @@
+/**
+ *
+ * @author 莫那·鲁道
+ * @date 2019-01-26-11:04
+ */
+package thinkinjava.event;
+/**
+ * 类似 guava 的 EventBus 设计, 通过机制解耦模块之间的耦合关系.
+ * */
